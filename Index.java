21f3cf370cb155408ae241398fe92373e96867a3
Henrique Sales <71890654+HenriqueSales10@users.noreply.github.com>
2024-02-23 22:14:28 -0300

    Update Index.java

--- Index.java
+++ Index.java
@@ -4,31 +4,36 @@ public class Index {
     public static void main (String[]args) {                                                             
         Scanner menu = new Scanner(System.in);                                                             
         // 1
-        System.out.print("##--Teste--##\n\n");
-        System.out.print("|-----------------------------|\n");
+        System.out.print("##---------- Exercicios 23 - 02 ----------##\n\n");
+        System.out.print("|------------------------------------------|\n");
         System.out.print("| Opção 1 - Determinar se um ano é bissexto|\n");
-        System.out.print("| Opção 2 - Sequência de Fibonnachi|\n");
-        System.out.print("| Opção 3 -        |\n");
-        System.out.print("| Opção 4 - Solicitar a temperatura|\n");
-        System.out.print("| Opção 5 - Verificar idade|\n");
-        System.out.print("|-----------------------------|\n");
+        System.out.print("| Opção 2 - Sequência de Fibonnachi        |\n");
+        System.out.print("| Opção 3 - Mostrar tabuada                |\n");
+        System.out.print("| Opção 4 - Solicitar a temperatura        |\n");
+        System.out.print("| Opção 5 - Verificar idade                |\n");
+	System.out.print("| Opção 6 - Sair                           |\n");
+        System.out.print("|------------------------------------------|\n");
         System.out.print("Digite uma opção: ");
 
         int opcao = menu.nextInt();
 
         switch (opcao) {
         case 1:
-            System.out.print("Digite o ano");
+            System.out.println("Digite o ano");
             int ano = menu.nextInt();
             System.out.print("O ano é bissexto? " + AnoBissexto(ano));
             break;
 
         case 2:
-            System.out.print("\n \n");
+            System.out.println("\n Digite o número desejado \n");
+            int numero = menu.nextInt();
+	    System.out.println("O " + numero + "º número na sequência de Fibonacci é: " + Fibonacci(numero));
             break;
 
         case 3:
-            System.out.print("\n \n");
+            System.out.println("\n Digite o número desejado para calcular a tabuada.\n");
+ 	    int numero = menu.nextInt();
+	    CalcularTabuada(numero);
             break;
 
         case 4:
@@ -62,5 +67,22 @@ public class Index {
 			return true;
 		}
 		return false;
-	}                                                                                                                                                                                                                                                                
+	}  
+	
+        public static int Fibonacci(int numero) {
+        if (numero <= 1) {
+            return n;
+        } else {
+            return Fibonacci(numero - 1) + fibonacci(numero - 2);
+        }
+
+	public static void CalcularTabuada(int numero{
+        System.out.println("Tabuada do " + numero + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+    	}
+    }                                                                
 }                                                                                           
